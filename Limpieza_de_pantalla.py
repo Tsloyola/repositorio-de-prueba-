b8452c3135f0dd3baed4e64fe4f2362434944407
Tomás Loyola <100977113+Tsloyola@users.noreply.github.com>
2022-03-11 19:40:26 -0300

    Delete Limpieza_de_pantalla.py

--- Limpieza_de_pantalla.py
+++
@@ -1,9 +0,0 @@
-#Limpieza de pantalla
-
-def cleaning():
-    if sys.platform.startswith('win'):
-        os.system('cls')
-    elif sys.platform.startswith('darwin'):
-        os.system('clear')
-    elif sys.platform.startswith('linux'):
-        os.system('clear')
