458f054db12a9fe58978e8a25463e145670a8e97
Tsloyola <100977113+Tsloyola@users.noreply.github.com>
2022-03-11 19:16:01 -0300

    Add files via upload

---
+++ Limpieza_de_pantalla.py
@@ -0,0 +1,9 @@
+#Limpieza de pantalla
+
+def cleaning():
+    if sys.platform.startswith('win'):
+        os.system('cls')
+    elif sys.platform.startswith('darwin'):
+        os.system('clear')
+    elif sys.platform.startswith('linux'):
+        os.system('clear')
